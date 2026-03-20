9cf97fa8908d389317294d6605197c36e02960da
Festac <fesjo2009@gmail.com>
2026-03-20 04:03:00 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,22 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  balance: { type: Number, default: 0.99 }, // welcome bonus
-  isAdmin: { type: Boolean, default: false }
+  balance: { type: Number, default: 0 },
+  pin: String,
+  transactions: [
+    {
+      type: { type: String },
+      email: String,
+      amount: Number,
+      date: { type: Date, default: Date.now }
+    }
+  ]
 });
 
 module.exports = mongoose.model("User", userSchema);
-
-transactions: [
-  {
-    type: { type: String }, // "sent" or "received"
-    email: String,
-    amount: Number,
-    date: { type: Date, default: Date.now }
-  }
-]
-
-pin: {
-  type: String
-}
-
