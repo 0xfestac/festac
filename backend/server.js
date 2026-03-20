d9db9347fb5860172677a0f0db93ea38da460fb9
Festac <fesjo2009@gmail.com>
2026-03-20 11:18:23 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,5 +39,15 @@ app.listen(5000, () => {
   console.log("🚀 FESTAC is up and running on port 5000");
 });
 
+const path = require("path");
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Catch-all route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
 const cors = require("cors");
 app.use(cors());
