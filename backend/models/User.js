7410a74e07a1a6abc002b855911e3185cb12dec2
Festac <fesjo2009@gmail.com>
2026-03-20 08:16:28 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,24 +14,5 @@ const userSchema = new mongoose.Schema({
     }
   ]
 });
-const mongoose = require("mongoose");
-
-const transactionSchema = new mongoose.Schema({
-  type: String, // "credit" or "debit"
-  amount: Number,
-  to: String,
-  from: String,
-  date: { type: Date, default: Date.now }
-});
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  balance: { type: Number, default: 0 },
-  pin: String,
-  transactions: [transactionSchema]
-});
-
-module.exports = mongoose.model("User", userSchema);
 
 module.exports = mongoose.model("User", userSchema);
