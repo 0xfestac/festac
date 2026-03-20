5f616b79727ad2a7c811a32907c0d968740cb3fd
Festac <fesjo2009@gmail.com>
2026-03-20 01:32:41 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const SECRET = "festac_secret";
+const SECRET = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
